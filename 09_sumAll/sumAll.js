0e48a07211b12a194e674f76c45126c5fd05b79b
DavidOh1606 <david.oh1606@gmail.com>
2025-08-11 14:58:34 -0700

    Implement sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    if (one < 0 || two < 0 || !Number.isInteger(one + two)) return "ERROR";
 
+    let sum = 0;
+
+    let start;
+    let end;
+
+    if (one > two) {
+        start = two;
+        end = one;
+    }
+
+    else {
+        start = one;
+        end = two;
+    }
+
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
