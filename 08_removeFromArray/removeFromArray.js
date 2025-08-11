d198571dded223995f60efdd8f3cd095b67d6b6d
DavidOh1606 <david.oh1606@gmail.com>
2025-08-11 14:49:07 -0700

    Implement removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    for (let i = 0; i < args.length; i++) {
+        let index = arr.findIndex(value => value === args[i]);
+
+        if (index != -1) {
+            arr.splice(index, 1);
+        }
+
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
