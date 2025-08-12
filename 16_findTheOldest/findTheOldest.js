20c70d68c29b8c5139b1e967d95e2524606bb69c
DavidOh1606 <david.oh1606@gmail.com>
2025-08-12 15:23:54 -0700

    implement findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    const oldestPerson = people.reduce((oldest, curr) => {
+        let oldestDeathYear = oldest.yearOfDeath;
+        let currDeathYear = curr.yearOfDeath;
+
+        if (!("yearOfDeath" in oldest)) {
+            oldestDeathYear = currentYear;
+        }
+
+        if (!("yearOfDeath" in curr)) {
+            currDeathYear = currentYear;
+        }
+
+
+        if ((currDeathYear - curr.yearOfBirth) > (oldestDeathYear - oldest.yearOfBirth)) {
+            oldest = curr;
+        }
+
+        return oldest;
+    }, people[0]);
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
