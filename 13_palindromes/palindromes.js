fed988e040c63f810845127c39d2db81aaca67ec
DavidOh1606 <david.oh1606@gmail.com>
2025-08-12 14:38:31 -0700

    Implement palindromes method

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9\s]/g, '').replaceAll(' ', '').toLowerCase();
 
+    let start = 0;
+    let end = string.length - 1;
+
+    while (start < end) {
+        if (string[start] != string[end]) {
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
